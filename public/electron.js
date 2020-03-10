5a2726da30f209250043d93349cd685c8a13502b
Toby56 <Toby56@pm.me>
2020-03-10 19:20:36 +1100

    Format code with maximum line length

--- public/electron.js
+++ public/electron.js
@@ -1,6 +1,9 @@
 const { app, BrowserWindow, Tray, Menu, shell } = require("electron");
 const path = require("path");
 const fs = require("fs");
+require("electron-unhandled")();
+
+const isDev = !app.isPackaged;
 
 let win = null;
 
@@ -15,10 +18,13 @@ if (!app.requestSingleInstanceLock()) {
   });
 
   app.whenReady().then(() => {
-    const isDev = !app.isPackaged;
-
     console.log("Launching window");
-    const { width, height, x, y } = (windowStateKeeper = require("electron-window-state")({
+    const {
+      width,
+      height,
+      x,
+      y
+    } = (windowStateKeeper = require("electron-window-state")({
       defaultWidth: 800,
       defaultHeight: 600
     }));
@@ -45,9 +51,14 @@ if (!app.requestSingleInstanceLock()) {
 
     windowStateKeeper.manage(win);
 
-    contents.on("did-fail-load", (event, errorCode, errorDescription, validatedURL) => {
-      console.error(`Could not load URL ${validatedURL}: ${errorCode} ${errorDescription}`);
-    });
+    contents.on(
+      "did-fail-load",
+      (event, errorCode, errorDescription, validatedURL) => {
+        console.error(
+          `Could not load URL ${validatedURL}: ${errorCode} ${errorDescription}`
+        );
+      }
+    );
 
     contents.on("did-finish-load", () => {
       console.log("Finished loading");
@@ -73,54 +84,87 @@ if (!app.requestSingleInstanceLock()) {
       shell.openExternal(url);
     });
 
+    contents.executeJavaScript(
+      `document.body.insertAdjacentHTML("afterBegin", \`${fs
+        .readFileSync(
+          isDev
+            ? "public/electron.html"
+            : "resources/app.asar/build/electron.html"
+        )
+        .toString()}\`)`
+    );
     console.log("Waiting for dom ready");
     contents.on("dom-ready", () => {
       console.log("Injecting CSS, HTML and JavaScript");
-      contents.insertCSS(fs.readFileSync(isDev ? "public/electron.css" : "resources/app.asar/build/electron.css").toString());
-      contents.executeJavaScript(`document.body.insertAdjacentHTML("afterBegin", \`${fs.readFileSync(isDev ? "public/electron.html" : "resources/app.asar/build/electron.html").toString()}\`)`);
+      contents.insertCSS(
+        fs
+          .readFileSync(
+            isDev
+              ? "public/electron.css"
+              : "resources/app.asar/build/electron.css"
+          )
+          .toString()
+      );
       contents
         .executeJavaScript(
           `(${() => {
             try {
               global.win = require("electron").remote.getCurrentWindow();
 
-              document.getElementById("titlebar-text").innerHTML = document.title;
+              document.getElementById("titlebar-text").innerHTML =
+                document.title;
               win.on("page-title-updated", (event, title) => {
                 document.getElementById("titlebar-text").innerHTML = title;
               });
 
-              document.getElementById("titlebar-button-minimize").addEventListener("click", event => {
-                win.minimize();
-              });
+              document
+                .getElementById("titlebar-button-minimize")
+                .addEventListener("click", event => {
+                  win.minimize();
+                });
 
-              document.getElementById("titlebar-button-maximize").addEventListener("click", event => {
-                win.maximize();
-              });
+              document
+                .getElementById("titlebar-button-maximize")
+                .addEventListener("click", event => {
+                  win.maximize();
+                });
 
-              document.getElementById("titlebar-button-restore").addEventListener("click", event => {
-                win.isFullScreen() ? win.setFullScreen(false) : win.unmaximize();
-              });
+              document
+                .getElementById("titlebar-button-restore")
+                .addEventListener("click", event => {
+                  win.isFullScreen()
+                    ? win.setFullScreen(false)
+                    : win.unmaximize();
+                });
 
-              document.getElementById("titlebar-button-close").addEventListener("click", event => {
-                win.close();
-              });
+              document
+                .getElementById("titlebar-button-close")
+                .addEventListener("click", event => {
+                  win.close();
+                });
 
               function toggleMaximized() {
-                win.isMaximized() ? document.body.classList.add("maximized") : document.body.classList.remove("maximized");
+                win.isMaximized()
+                  ? document.body.classList.add("maximized")
+                  : document.body.classList.remove("maximized");
               }
               toggleMaximized();
               win.on("maximize", toggleMaximized);
               win.on("unmaximize", toggleMaximized);
 
               function toggleBlurred() {
-                win.isFocused() ? document.body.classList.remove("blurred") : document.body.classList.add("blurred");
+                win.isFocused()
+                  ? document.body.classList.remove("blurred")
+                  : document.body.classList.add("blurred");
               }
               toggleBlurred();
               win.on("blur", toggleBlurred);
               win.on("focus", toggleBlurred);
 
               function toggleFullScreen() {
-                win.isFullScreen() ? document.body.classList.add("full-screen") : document.body.classList.remove("full-screen");
+                win.isFullScreen()
+                  ? document.body.classList.add("full-screen")
+                  : document.body.classList.remove("full-screen");
               }
               toggleFullScreen();
               win.on("enter-full-screen", toggleFullScreen);
@@ -135,10 +179,10 @@ if (!app.requestSingleInstanceLock()) {
           }})()`
         )
         .then(result => {
-          if (result) {
-            if (result instanceof Error) {
-              console.error(`Error in web contents: ${result}`);
-            }
+          if (result instanceof Error) {
+            console.error(`Error in web contents: ${result}`);
+          } else {
+            console.log("Success");
           }
         });
     });
