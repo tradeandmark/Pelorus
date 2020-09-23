aff889393246de9172f077b4645cbceb93f65871
Toby56 <Toby56@pm.me>
2020-09-23 21:23:57 +1000

    Make it Typescript!

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App/";
+import App from "./App";
 import { FluentCustomizations } from "@uifabric/fluent-theme";
 import { Customizer } from "office-ui-fabric-react";
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <Customizer {...FluentCustomizations}>
@@ -16,4 +16,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
